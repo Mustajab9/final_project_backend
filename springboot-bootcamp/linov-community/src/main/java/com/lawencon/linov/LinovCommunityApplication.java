4bb5dfd821c3788480b271e69f058bb00e4f3811
48911414+CardinalSAO@users.noreply.github.com <48911414+CardinalSAO@users.noreply.github.com>
2022-03-22 14:47:33 +0700

    init Project

---
+++ springboot-bootcamp/linov-community/src/main/java/com/lawencon/linov/LinovCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.lawencon.linov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinovCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinovCommunityApplication.class, args);
+	}
+
+}
---
+++ springboot-bootcamp/linov-community/src/test/java/com/lawencon/linov/LinovCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lawencon.linov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinovCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
