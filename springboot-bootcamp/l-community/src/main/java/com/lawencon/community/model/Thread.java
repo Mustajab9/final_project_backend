47bd397c7718c92efd0b39dcc42a8a167df0eab4
wisnu <awisnu56@gmail.com>
2022-03-23 13:30:23 +0700

    add premium on thread model

--- springboot-bootcamp/l-community/src/main/java/com/lawencon/community/model/Thread.java
+++ springboot-bootcamp/l-community/src/main/java/com/lawencon/community/model/Thread.java
@@ -1,13 +1,42 @@
 package com.lawencon.community.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
 import com.lawencon.base.BaseEntity;
 
+@Entity
+@Table(name = "threads", uniqueConstraints = {
+		@UniqueConstraint(
+				name="thread_bk",
+				columnNames = "thread_code")
+})
 public class Thread extends BaseEntity {
 	
 	private static final long serialVersionUID = -919610429872056255L;
+	
+	@Column(name = "thread_title", length=100)
 	private String threadTitle;
+	
+	@Column(name = "thread_code", length=5)
 	private String threadCode;
+	
+	@Column(name = "thread_content", columnDefinition = "DEFAULT text")
 	private String threadContent;
+	
+	@Column(name = "is_premium")
+	private Boolean isPremium = false;
+	
+	@ManyToOne
+	@JoinColumn(name = "category_id")
+	private Category categoryId;
+	
+	@ManyToOne
+	@JoinColumn(name = "type_id")
 	private ThreadType typeId;
 	
 	public String getThreadTitle() {
@@ -27,6 +56,18 @@ public class Thread extends BaseEntity {
 	}
 	public void setThreadContent(String threadContent) {
 		this.threadContent = threadContent;
+	}
+	public Boolean getIsPremium() {
+		return isPremium;
+	}
+	public void setIsPremium(Boolean isPremium) {
+		this.isPremium = isPremium;
+	}
+	public Category getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(Category categoryId) {
+		this.categoryId = categoryId;
 	}
 	public ThreadType getTypeId() {
 		return typeId;
